708a3ae9029a18b31acb50d2cbb64ed46a821a18
Fernando Nicolás <fersanti2896@gmail.com>
2023-11-14 03:15:54 -0600

    Proyecto desde S10.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Aplicación de Monitoreo - MongoDB y PostgreSQL
+
+Se aplica la integración de base de datos con MongoDB y PostgreSQL en nuestra aplicación de `NOC`, los temas que se ven son:
+
+1. Uso de `Mongoose`, `Prisma` y `TypeORM`.
+2. Migraciones de prisma
+3. Insertar en base de datos
+4. Mapeo de `data` a Entidades
+5. Creación de `datasources`.
+
+### Para iniciar proyecto
+
+1. Instalar las dependencias. 
+```
+npm install
+```
+2. Renombrar el archiv `.env.template` a `.env` y definir las variables de entorno.
+3. Ejecutar la aplicación. 
+```
+npm run dev
+```
+
+
+
---
+++ src/presentation/server.ts
@@ -0,0 +1,35 @@
+import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
+import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
+import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
+import { CronService } from './cron/cron-service';
+import { EmailService } from './email/email.service';
+
+/* Instancias */
+const fileSystemLogRepository = new LogRepositoryImpl( 
+    new FileSystemDatasource()
+);
+
+const emailService = new EmailService();
+
+export class Server {
+    public static start() {
+        console.log('Server started...')
+
+        /* Mandando email */
+        // new SendEmailLogs(emailService, fileSystemLogRepository).execute(['fersanti2896@gmail.com']);
+        
+        // CronService.createJob( 
+        //     '*/5 * * * * *',
+        //     () => {
+        //         const url = 'https://google.com';
+
+        //         new CheckService(
+        //             fileSystemLogRepository,
+        //             () => console.log(`${ url } is ok`),
+        //             ( error ) => console.log(error)
+        //         ).execute( url );
+        //         // new CheckService().execute('http://localhost:3000');
+        //     }
+        // );
+    }   
+}
